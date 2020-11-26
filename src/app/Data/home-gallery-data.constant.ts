7b18be3b8caa94f16e90de6832dbeaa83a342d62
Muhammad Moiz <muhammadmoiz0087@gmail.com>
2020-11-26 13:29:29 +0500

    fixess

--- src/app/Data/home-gallery-data.constant.ts
+++ src/app/Data/home-gallery-data.constant.ts
@@ -2,25 +2,34 @@ import { IImages } from '../typing/client/image';
 
 export const MockDataHomeGallery: Array<IImages> = [
     {
-        img: 'assets/gallery/DSC_5156.JPG',
+        img: 'assets/gallery/DSC_5176.JPG',
     },
     {
-        img: 'assets/gallery/DSC_5171.JPG',
+        img: 'assets/gallery/DSC_5690.JPG',
     },
     {
-        img: 'assets/gallery/DSC_5161.JPG',
+        img: 'assets/gallery/DSC_5391.JPG',
     },
     {
         img: 'assets/gallery/DSC_5182.JPG',
     },
     {
-        img: 'assets/gallery/DSC_5169.JPG',
+        img: 'assets/gallery/DSC_5687.JPG',
+    },
+    {
+        img: 'assets/gallery/DSC_5386.JPG',
+    },
+    {
+        img: 'assets/gallery/DSC_5171.JPG',
     },
     {
         img: 'assets/gallery/DSC_5183.JPG',
     },
     {
-        img: 'assets/gallery/DSC_5176.JPG',
+        img: 'assets/gallery/DSC_5389.JPG',
+    },
+    {
+        img: 'assets/gallery/DSC_5709.JPG',
     },
     {
         img: 'assets/gallery/DSC_5165.JPG',
